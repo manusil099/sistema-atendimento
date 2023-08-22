d2d0243972e8b7dc65150e4903043237d97a287b
Manuele da Silva <manuele.silva2@estudante.ifms.edu.br>
2023-08-21 22:43:01 -0400

    add funções e eventos

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,15 @@ const ulLista = document.querySelector('#ul-lista');
 const displayNome= document.querySelector('#display-nome');
 const btnChamar = querySelector('#btn-chamar');
 const listadeAtendimento = new Array();
+
+function incluir() {
+    listadeAtendimento.push(inputNome.value)
+}
+
+function listar() {
+  for(let nome of listadeAtendimento){
+    console.log(nome)
+  }
+}
+//eventos
+btnIncluir.onckick = incluir
